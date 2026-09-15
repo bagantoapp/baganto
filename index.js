e78d19a76df808364544981e7e5e683fa6ba9702
kirana8867 <kirangowda3636@gmail.com>
2026-09-15 20:28:49 +0530

    Lock down CORS to baganto.com only

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: 'https://baganto.com' }));
 const bcrypt = require('bcrypt');
 
 app.use(express.json());
